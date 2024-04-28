1e5f9f19e7f389c6f95113f05298d045d15be2d2
Braedan Royer <royerbraedan@gmail.com>
2024-04-28 13:55:30 -0700

    began working on the javascript. added a set timeout to the modal, and added functionality to the close modal button

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,10 @@
-ECHO is on.
+const modal = document.querySelector('#modal')
+const closeModalBtn = document.querySelector('#close-modal-btn')
+
+setTimeout(function() {
+    modal.style.display = 'inline'
+}, 1500)
+
+closeModalBtn.addEventListener("click", () => {
+    closeModalBtn.style.display = 'none'
+})
