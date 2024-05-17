36b55b5876cf876194563a46b9c4583d4acd0a5a
Braedan Royer <royerbraedan@gmail.com>
2024-05-17 13:05:04 -0700

    redid the project

--- JS/index.js
+++ JS/index.js
@@ -1,45 +1,44 @@
-const modal = document.querySelector('#modal')
-const closeModalBtn = document.querySelector('#close-modal-btn')
-const modalDeclineBtn = document.querySelector("#dmodal-decline-btn")
-const modalForm = document.querySelector("#modal-form")
-const modalText = document.querySelector("#modal-text")
-const modalChoiceBtns = document.querySelector("#modal-choice-btns")
+const modal = document.querySelector("#modal");
+const modalCloseBtn = document.querySelector("#modal-close-btn");
+const modalForm = document.querySelector(".modal-form");
+const modalText = document.querySelector("#modal-text");
+const modalChoiceBtnsDiv = document.querySelector("#modal-choice-btns")
+const declineBtn = document.querySelector("#decline-btn")
 
+setTimeout(() => {
+  modal.style.display = "block";
+}, 1500);
 
+modalCloseBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-setTimeout(function() {
-    modal.style.display = 'inline'
-}, 1500)
-
-closeModalBtn.addEventListener("click", () => {
-    modal.style.display = 'none'
+declineBtn.addEventListener("mouseenter", () => {
+    modalChoiceBtnsDiv.classList.toggle("reverse")
 })
 
-modalDeclineBtn.addEventListener("click", () => {
-    modalChoiceBtns.classList.toggle("reverse")
-})
-
-modalForm.addEventListener("submit", function(e) {
-    e.preventDefault()
-    const userData = new FormData(modalForm)
-    const name = userData.get("username")
-    modalText.innerHTML = `
-    <div class="modal-inner-loading">
-        <img src="IMG/loading.svg" class="loading">
-        <p id="uploadText">
-            Uploading your data to the dark web...
-        </p>
-    </div>`
-    setTimeout(function() {
-        document.querySelector("#uploadText").innerText = `
-        Making the sale...` 
-    }, 1500)
-    setTimeout(function() {
-        closeModalBtn.disabled = false
-        document.querySelector("#modal-inner").innerHTML = `
-        <h2>Thanks, <span id="modal-display-name">${name}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
-        ` 
-    }, 3000)
-})
-
+modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const modalFormData = new FormData(modalForm);
+  const username = modalFormData.get("username");
+  modalText.innerHTML = `
+  <div class="modal-inner-loading">
+    <img src="/IMG/loading.svg" class="loading">
+    <p id="upload-text">
+        Uploading your data to the dark web...
+    </p>
+</div>`;
+  setTimeout(() => {
+    document.querySelector("#upload-text").innerText = `Making the sale...`;
+  }, 1500);
+  setTimeout(() => {
+    document.querySelector("#modal-inner").innerHTML = `
+    <h2>Thanks you <span class="modal-display-name">${username}</span>, you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="/IMG/bugatti.jpg">
+    </div>
+    `;
+    modalCloseBtn.disabled = false;
+  }, 3000);
+});
